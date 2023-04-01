62990d771d551dc283d750302c523a3d30f2659d
Benjamin <benseed.bs@gmail.com>
2023-04-01 21:05:12 +0100

    changed API key and fixed CORS issue

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -4,10 +4,7 @@ import NewsList from "./NewsList";
 import { nanoid } from "nanoid";
 import { useState, useEffect } from "react";
 
-// const apiKey = import.meta.env.REACT_APP_NEWS_API_KEY;
-
-const url =
-  "https://newsdata.io/api/1/news?apikey=pub_1988571b251a34e1fbf266ae27a02805fb6e9&country=gb,us";
+const url = import.meta.env.VITE_NEWS_API_KEY;
 
 const News = () => {
   const [news, setNews] = useState([]);
