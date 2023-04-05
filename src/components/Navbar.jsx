46b09a0227c4eb738b2f497b1b61bbf768e4677e
Benjamin <benseed.bs@gmail.com>
2023-04-05 20:10:33 +0100

    added a button functionality to the navbar which toggles the links. added css which needs more work.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
   return (
-    <nav className="navbar">
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          {" "}
-          <Link to="/bbc">BBC</Link>
-        </li>
-        <li>
-          {" "}
-          <Link to="/UsNews">U.S.</Link>
-        </li>
-      </ul>
+    <nav>
+      {showLinks && (
+        <div className="navbar">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              {" "}
+              <Link to="/bbc">BBC</Link>
+            </li>
+            <li>
+              {" "}
+              <Link to="/UsNews">U.S.</Link>
+            </li>
+          </ul>
+        </div>
+      )}
+
+      <button className="navbar-btn" onClick={toggleLinks}>
+        <FaBars />
+      </button>
     </nav>
   );
 };
